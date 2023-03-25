4e8a9f54f731cc263b0df76d81fcf8389e6cd7de
johnson999666 <123405062+johnson999666@users.noreply.github.com>
2023-03-25 09:05:25 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -26,4 +26,14 @@ $(document).ready(function() {
       }
     }
   });
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
   
