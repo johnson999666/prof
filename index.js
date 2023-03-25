5b1c58aaf6f270b59217058355b887feb1d0e480
johnson999666 <123405062+johnson999666@users.noreply.github.com>
2023-03-25 09:53:59 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
-$(document).ready(function() {
-    $('#slideshow-container').addClass('slideshow');
-  });
-  
 
-  var touchstartX = 0;
-  var touchendX = 0;
-  
-  $('.slideshow').on('touchstart', function(event) {
-    touchstartX = event.changedTouches[0].screenX;
-  });
-  
-  $('.slideshow').on('touchend', function(event) {
-    touchendX = event.changedTouches[0].screenX;
-    if (touchendX < touchstartX) {
-      slideIndex++;
-      $('.slideshow').css('transform', 'translateX(-' + slideIndex * 33.33 + '%)');
-      if (slideIndex >= 2) {
-        slideIndex = -1;
-      }
-    } else if (touchendX > touchstartX) {
-      slideIndex--;
-      $('.slideshow').css('transform', 'translateX(-' + slideIndex * 33.33 + '%)');
-      if (slideIndex <= -1) {
-        slideIndex = 2;
-      }
-    }
-  });
-  var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('.swiper-container', {
     loop: true,
     autoplay: {
       delay: 3000,
@@ -35,5 +8,6 @@ $(document).ready(function() {
       el: '.swiper-pagination',
       clickable: true,
     },
+    freeMode: true, // enable free mode scrolling
   });
   
